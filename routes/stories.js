2e6e5c13db96b96fb97226b56c23579c038b5836
sooaaib <soaib.safi@gmail.com>
2020-09-20 16:55:54 +0200

    :construction:Handle routes

--- routes/stories.js
+++ routes/stories.js
@@ -41,6 +41,27 @@ router.get("/", ensureAuth, async (req, res) => {
     }
 });
 
+// @desc    Show single story
+// @route   GET/ /stories/:id
+router.get("/:id", ensureAuth, async(req, res) => {
+    try {
+        let story = await Story.findById(req.params.id)
+        .populate('user')
+        .lean()
+
+        if(!story){
+            return res.render('error/400')
+        }
+
+        res.render('stories/show', {
+            story,
+        })
+    } catch (err) {
+        console.log(err);
+        res.render("/error/404");
+    }
+});
+
 // @desc    Show edit page
 // @route   GET/ /stories/edit/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
@@ -100,6 +121,26 @@ router.delete("/:id", ensureAuth, async (req, res) => {
         res.redirect("/dashboard");
     } catch (err) {
         console.log(err);
+        res.render("error/500");
+    }
+});
+
+// @desc    User Stories
+// @route   GET /stories/user/:userId
+router.get("/user/:userId", ensureAuth, async(req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public'
+        })
+        .populate('user')
+        .lean()
+
+        res.render('stories/index', {
+            stories,
+        })
+    } catch (err) {
+        console.log(err)
         res.render("error/500");
     }
 });
